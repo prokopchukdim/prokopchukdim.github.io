a1412e38dba11ae988a88f102fe2df1df03405ab
prokopchukdim <87666671+prokopchukdim@users.noreply.github.com>
2021-08-06 21:31:01 +0300

    Implemented project buttons

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var menuOpen = false;
 // Event listeners for once the window is loaded
 $(document).ready(function(){
   
+  //Fix project card button location
+  projectCardReposition();
+
   //About Me scroll-sensitive card control
   createAboutObserver();
 
@@ -21,6 +24,10 @@ $(document).ready(function(){
   //Fix glitches with menu display upon resize due to use of "slideUp" and "slideDown"
   var mobileThreshold = 843;
   $(window).on("resize", function(){
+
+    //Fix project card button location
+    projectCardReposition();
+
     if(menuOpen && $(this).width() > mobileThreshold){
       if($(window).scrollTop() < 64){
         $("#navbar").removeClass("navbar-not-transparent")
@@ -95,5 +102,53 @@ function handleAbout(entries, observer){
     else{
       $("#about-text").removeClass("card-hover-manual");
     }
+  })
+}
+
+//Repositions buttons under project cards to make better use of the space. Called upon load and viewport resize.
+function projectCardReposition(){
+  var buttons = new Array();
+  $(".project-button").each(function(){
+    buttons.push($(this));
+    
+  });
+
+  buttons.forEach(button => {
+    var requiredMargin = 15;
+    
+    var card = button.closest(".project-box");
+    var imageHeight = parseFloat(card.find(".project-image").css("height"));
+    var titleHeight = parseFloat(card.find(".project-title").css("height"));
+    var textHeight = parseFloat(card.find(".project-text").css("height"));
+    var buttonHeight = parseFloat(button.css("height"));
+
+    var paddingOffset = 53;
+    var ttbCombined = titleHeight + textHeight + buttonHeight + paddingOffset;
+
+    var textWidth = parseFloat(card.find(".project-text").css("width"));
+    card.find(".project-button-wrapper").css("width",textWidth);
+
+    if(textWidth <= 300 && $(window).width() <= 699){
+      button.css("width", "95%");
+      card.find(".project-button-divider").css("display","none");
+    }
+    else if(textWidth <= 300 && $(window).width() >= 699){
+      card.find(".project-button-divider").css("width",0);
+      card.find(".project-button-divider").css("display","inline-block");
+    }
+    else{
+      button.css("width", "42.5%");
+      card.find(".project-button-divider").css("display","inline-block");
+    }
+
+    //button.css("width", 0.42 * textWidth)
+
+    if (ttbCombined < imageHeight && $(window).width() >= 699){
+      requiredMargin = imageHeight - ttbCombined;
+      if (requiredMargin < 15){
+        requiredMargin = 15;
+      }
+    }
+    button.css("margin-top", requiredMargin);
   })
 }
